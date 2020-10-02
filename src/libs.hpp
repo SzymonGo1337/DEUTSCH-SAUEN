8e87cc07030e7ecf323096ca703868433468e50f
Szymon <goszymon1337@gmail.com>
2020-10-02 22:09:55 +0200

    shaders

---
+++ src/libs.hpp
@@ -0,0 +1,14 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <glm/glm.hpp>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <SOIL/SOIL.h>
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,91 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
+#include "libs.hpp"
+
+bool loadShaders(GLuint &program) {
+    bool loadSuccess = true;
+    char infoLog[512];
+    GLint success;
+
+    std::string temp = "";
+    std::string src = "";
+
+    std::ifstream in_file;
+
+    in_file.open("vertex_core.glsl");
+
+    if(in_file.is_open()) {
+        while(std::getline(in_file, temp)) {
+            src += temp + "\n";
+        }
+    } else {
+        std::cerr << "SHADER ERROR COULDN'T LOAD VERTEX FILE" << "\n";
+        loadSuccess = false;
+    }
+
+    in_file.close();
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const GLchar* vertSrc = src.c_str();
+    glShaderSource(vertexShader, 1, &vertSrc, NULL);
+    glCompileShader(vertexShader);
+
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cerr << "SHADER ERROR COULDN'T COMPILE VERTEX FILE" << "\n";
+        std::cerr << infoLog << "\n";
+        loadSuccess = false;
+    }
+
+    temp = "";
+    src = "";
+
+    in_file.open("fragment_core.glsl");
+
+    if(in_file.is_open()) {
+        while(std::getline(in_file, temp)) {
+            src += temp + "\n";
+        }
+    } else {
+        std::cerr << "SHADER ERROR COULDN'T LOAD FRAGMENT FILE" << "\n";
+        loadSuccess = false;
+    }
+
+    in_file.close();
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    const GLchar* fragSrc = src.c_str();
+    glShaderSource(fragmentShader, 1, &fragSrc, NULL);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cerr << "SHADER ERROR COULDN'T COMPILE FRAGMENT FILE" << "\n";
+        std::cerr << infoLog << "\n";
+        loadSuccess = false;
+    }
+
+    program = glCreateProgram();
+
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+
+    glLinkProgram(program);
+
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(program, 512, NULL, infoLog);
+        std::cerr << "SHADER ERROR COULDN'T COMPILE PROGRAM FILE" << "\n";
+        std::cerr << infoLog << "\n";
+        loadSuccess = false;
+    }
+
+    glUseProgram(0);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return loadSuccess;
+}
 
 int main() {
     glfwInit();
@@ -33,10 +118,15 @@ int main() {
         glfwTerminate();
     }
 
+    GLuint core_program;
+    if(!loadShaders(core_program)) {
+        glfwTerminate();
+    }
+
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
 
